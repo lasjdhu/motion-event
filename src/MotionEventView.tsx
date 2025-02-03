5a6a813ddb8d1b9e12871d8ec0f48f40adff9a00
Dmitrii Ivanushkin <dmitrii.unwritten183@passmail.net>
2025-02-04 00:35:36 +0100

    feat: add minimal functionality (#11)
    Removed boilerplate, ios and web support, provided minimal functionality
    
    fix #2 #3 #4

--- src/MotionEventView.tsx
+++ src/MotionEventView.tsx
@@ -1,10 +1,10 @@
-import { requireNativeView } from 'expo';
-import * as React from 'react';
+import { requireNativeView } from "expo";
+import * as React from "react";
 
-import { MotionEventViewProps } from './MotionEvent.types';
+import { MotionEventViewProps } from "./MotionEvent.types";
 
 const NativeView: React.ComponentType<MotionEventViewProps> =
-  requireNativeView('MotionEvent');
+  requireNativeView("MotionEvent");
 
 export default function MotionEventView(props: MotionEventViewProps) {
   return <NativeView {...props} />;
